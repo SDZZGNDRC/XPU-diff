6c19d418ba08eb0847e3f4f0a8128a6318dda92c
SDZZGNDRC <SDZZGNDRC@gmail.com>
2022-09-01 16:41:31 +0800

    change the REFRESH_CYCLE

--- app/vgatest/csrc/main.c
+++ app/vgatest/csrc/main.c
@@ -3,7 +3,7 @@ int main()
 {
     uint32 a = 0x561b12;
     volatile uint32* pixel_p = (uint32*)VMEM_ADDR_BASE;
-    for(int i = 240; i < 300; i++)
+    for(int i = 0; i < 480; i++)
     {
         for(int j = 0; j < 640; j++)
         {
